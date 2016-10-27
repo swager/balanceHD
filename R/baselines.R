f54f21c30e96883a316134fd0d4b47404cc00d5e
Stefan Wager <swager@users.noreply.github.com>
2016-10-27 17:07:59 -0400

    Add standard error estimates to baseline methods (#1)
    * IPW SE
    
    * sandwich inference for BCH
    
    * minor
    
    * minor
    
    * minor
    
    * fix rank deficiency problem with BCH
    
    * fix

--- R/baselines.R
+++ R/baselines.R
@@ -7,9 +7,15 @@
 #' @return ATE estimate
 #'
 #' @export naive.ate
-naive.ate = function(X, Y, W) {
+naive.ate = function(X, Y, W, estimate.se = FALSE) {
 	tau.hat = mean(Y[W==1]) - mean(Y[W==0])
-	return(tau.hat)
+	
+	if (!estimate.se) {
+		return(tau.hat)
+	} else {	
+		se.hat = sqrt(var(Y[W==0]) / (sum(W==0) - 1) + var(Y[W==1]) / (sum(W==1) - 1))
+		return(c(tau.hat, se.hat))
+	}
 }
 
 #' Elastic net correction for confounding variables
@@ -25,7 +31,7 @@ naive.ate = function(X, Y, W) {
 #' @return ATE estimate
 #'
 #' @export elnet.ate
-elnet.ate = function(X, Y, W, target.pop=c(0, 1), alpha = 0.9) {
+elnet.ate = function(X, Y, W, target.pop=c(0, 1), alpha = 0.9, estimate.se = FALSE) {
 
 	# we want ATE for these indices
 	target.idx = which(W %in% target.pop)
@@ -36,7 +42,16 @@ elnet.ate = function(X, Y, W, target.pop=c(0, 1), alpha = 0.9) {
 	pred0 = coef(fit0)[1] + X %*% coef(fit0)[-1]
 	pred1 = coef(fit1)[1] + X %*% coef(fit1)[-1]
 	
-	mean((pred1 - pred0)[target.idx])
+	tau.hat = mean((pred1 - pred0)[target.idx])
+	
+	if (!estimate.se) {
+		return(tau.hat)
+	} else {	
+		var0 = var((Y - pred0)[W==0]) / (sum(W==0) - 1)
+		var1 = var((Y - pred1)[W==1]) / (sum(W==1) - 1)
+		se.hat = sqrt(var0 + var1)
+		return(c(tau.hat, se.hat))
+	}
 }
 
 #' Refit lasso correction for confounding variables
@@ -53,7 +68,7 @@ elnet.ate = function(X, Y, W, target.pop=c(0, 1), alpha = 0.9) {
 #' @return ATE estimate
 #'
 #' @export twostep.lasso.ate
-twostep.lasso.ate = function(X, Y, W, target.pop=c(0, 1), fit.propensity = TRUE) {
+twostep.lasso.ate = function(X, Y, W, target.pop=c(0, 1), fit.propensity = TRUE, estimate.se = FALSE) {
 
 	# we want ATE for these indices
 	target.idx = which(W %in% target.pop)
@@ -68,36 +83,48 @@ twostep.lasso.ate = function(X, Y, W, target.pop=c(0, 1), fit.propensity = TRUE)
 	}
 	
 	if (length(strong.coef) == 0) {
+		return(naive.ate(X, Y, W, estimate.se = TRUE))
+	}
+	
+	twostep.raw = lapply(0:1, function(ww) {
+		if (length(strong.coef) < 0.95 * sum(W == ww)) {
+			coefs = strong.coef
+		} else {
+			coefs = which(strong.coef %in% coef(list(lasso.fit0, lasso.fit1)[[1 + ww]])[-1] != 0)
+		}
+	
+		repeat {
+			print(coefs)
+			reg.df = data.frame(feat = X[,coefs], Y = Y, row.names = 1:nrow(X))
+			center = apply(reg.df[target.idx,,drop=FALSE], 2, mean)
+			center.df = data.frame(matrix(center, 1, ncol(reg.df)))
+			names(center.df) = names(reg.df)
+			fit.sel = lm(Y ~ ., data = reg.df[W == ww,,drop=FALSE])
+			if(sum(is.na(coef(fit.sel))) == 0) {
+				break
+			} else {
+				coefs = coefs[-sample.int(length(coefs), 1)]
+			}
+		}
 		
-		return(naive.ate(X, Y, W))
+		lm.pred = predict(fit.sel, newdata = center.df)
+	
+		if(estimate.se) {
+			M = sandwich::sandwich(fit.sel)
+			lm.var = c(1, center[-ncol(reg.df)]) %*% M %*%  c(1, center[-ncol(reg.df)]) 
+		} else {
+			lm.var = NA
+		}
 		
-	}
+		c(lm.pred, lm.var)
+	})
 	
-	reg.df = data.frame(feat = X[, strong.coef], Y = Y, row.names = 1:nrow(X))
-	
-	center = matrix(colMeans(reg.df[target.idx,]), 1, ncol(reg.df))
-	colnames(center) = names(reg.df)
-	center.df = data.frame(center)
-	
-	
-	if (length(strong.coef) < 0.95 * sum(W == 0)) {
-		fit0 = lm(Y ~ ., data = reg.df[W == 0,])
-		lm.pred0 = predict(fit0, newdata = center.df, se.fit=TRUE)
-		pred0 = c(fit=as.numeric(lm.pred0[[1]][1]), se.fit=lm.pred0[[2]][1])
+	tau.hat = twostep.raw[[2]][1] - twostep.raw[[1]][1]
+	var.hat = twostep.raw[[2]][2] + twostep.raw[[1]][2]
+
+	if(!estimate.se) {
+		tau.hat
 	} else {
-		lasso.pred0 = mean(predict(lasso.fit0, newx = X[target.idx,]))
-		pred0 = c(fit=lasso.pred0, se.fit=NA)
+		c(tau.hat, sqrt(var.hat))
 	}
-	
-	if (length(strong.coef) < 0.95 * sum(W == 1)) {
-		fit1 = lm(Y ~ ., data = reg.df[W == 1,])
-		lm.pred1 = predict(fit1, newdata = center.df, se.fit=TRUE)
-		pred1 = c(fit=as.numeric(lm.pred1[[1]][1]), se.fit=lm.pred1[[2]][1])
-	} else {
-		lasso.pred1 = mean(predict(lasso.fit1, newx = X[target.idx,]))
-		pred1 = c(fit=lasso.pred1, se.fit=NA)
-	}
-	
-	tau.hat = as.numeric(pred1["fit"] - pred0["fit"])
-	tau.hat
 }
