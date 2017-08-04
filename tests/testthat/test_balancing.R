e4304a3c4b3658004e38245be6a2583b72cba14a
Stefan Wager <stefan.wager@gmail.com>
2017-08-04 15:32:13 -0700

    Add dual-form POGS solver.

--- tests/testthat/test_balancing.R
+++ tests/testthat/test_balancing.R
@@ -13,13 +13,19 @@ gamma.mosek.positive2 = approx.balance(M, balance.target = balance.target, optim
 imbalance.mosek.positive2 = t(M) %*% gamma.mosek.positive - balance.target
 
 gamma.mosek.free2 = approx.balance(M, balance.target = balance.target, optimizer = "mosek", allow.negative.weights = TRUE, use.dual = TRUE, zeta = 0.5)
-imbalance.mosek.free2 = t(M) %*% gamma.mosek.free - balance.target
+imbalance.mosek.free2 = t(M) %*% gamma.mosek.free2 - balance.target
 
-gamma.pogs.positive = approx.balance(M, balance.target = balance.target, optimizer = "pogs", allow.negative.weights = FALSE, zeta = 0.5)
+gamma.pogs.positive = approx.balance(M, balance.target = balance.target, optimizer = "pogs", allow.negative.weights = FALSE, use.dual = FALSE, zeta = 0.5)
 imbalance.pogs.positive = t(M) %*% gamma.pogs.positive - balance.target
 
-gamma.pogs.free = approx.balance(M, balance.target = balance.target, optimizer = "pogs", allow.negative.weights = TRUE, zeta = 0.5)
+gamma.pogs.free = approx.balance(M, balance.target = balance.target, optimizer = "pogs", allow.negative.weights = TRUE, use.dual = FALSE, zeta = 0.5)
 imbalance.pogs.free = t(M) %*% gamma.pogs.free - balance.target
+
+gamma.pogs.positive2 = approx.balance(M, balance.target = balance.target, optimizer = "pogs", allow.negative.weights = FALSE, use.dual = TRUE, zeta = 0.5)
+imbalance.pogs.positive2 = t(M) %*% gamma.pogs.positive2 - balance.target
+
+gamma.pogs.free2 = approx.balance(M, balance.target = balance.target, optimizer = "pogs", allow.negative.weights = TRUE, use.dual = TRUE, zeta = 0.5)
+imbalance.pogs.free2 = t(M) %*% gamma.pogs.free2 - balance.target
 
 gamma.qp.positive = approx.balance(M, balance.target = balance.target, optimizer = "quadprog", allow.negative.weights = FALSE, zeta = 0.5)
 imbalance.qp.positive = t(M) %*% gamma.qp.positive - balance.target
@@ -31,6 +37,7 @@ test_that("positivity constraint works", {
   expect_true(all(gamma.mosek.positive >0))
   expect_true(all(gamma.mosek.positive2 > -10^(-8)))
   expect_true(all(gamma.pogs.positive > -10^(-4)))
+  expect_true(all(gamma.pogs.positive2 > -10^(-4)))
   expect_true(all(gamma.qp.positive > 0))
 })
 
@@ -41,6 +48,8 @@ test_that("removing positivity constraint improves objective", {
                 (sum(gamma.mosek.free2^2) + max(abs(imbalance.mosek.free2))^2))
   expect_true((sum(gamma.pogs.positive^2) + max(abs(imbalance.pogs.positive))^2) >
                 (sum(gamma.pogs.free^2) + max(abs(imbalance.pogs.free))^2))
+  expect_true((sum(gamma.pogs.positive2^2) + max(abs(imbalance.pogs.positive2))^2) >
+                (sum(gamma.pogs.free2^2) + max(abs(imbalance.pogs.free2))^2))
   expect_true((sum(gamma.qp.positive^2) + max(abs(imbalance.qp.positive))^2) >
                 (sum(gamma.qp.free^2) + max(abs(imbalance.qp.free))^2))
 })
@@ -51,7 +60,9 @@ test_that("gamma sums to 1", {
   expect_equal(sum(gamma.mosek.positive2), 1)
   expect_equal(sum(gamma.mosek.free2), 1)
   expect_equal(sum(gamma.pogs.positive), 1, tolerance = 5e-3)
-  expect_equal(sum(gamma.pogs.free), 1, tolerance = 5e-5)
+  expect_equal(sum(gamma.pogs.free), 1, tolerance = 5e-4)
+  expect_equal(sum(gamma.pogs.positive2), 1)
+  expect_equal(sum(gamma.pogs.free2), 1)
   expect_equal(sum(gamma.qp.positive), 1)
   expect_equal(sum(gamma.qp.free), 1)
 })
@@ -63,6 +74,8 @@ test_that("optimizers match", {
   expect_equal(max(abs(gamma.qp.positive - gamma.mosek.positive2)), 0, tolerance = 5e-4)
   expect_equal(max(abs(gamma.qp.free - gamma.pogs.free)), 0, tolerance = 5e-3)
   expect_equal(max(abs(gamma.qp.positive - gamma.pogs.positive)), 0, tolerance = 5e-2)
+  expect_equal(max(abs(gamma.qp.free - gamma.pogs.free2)), 0, tolerance = 5e-4)
+  expect_equal(max(abs(gamma.qp.positive - gamma.pogs.positive2)), 0, tolerance = 5e-3)
 })
 
 gamma.mosek.free.zeta9 = approx.balance(M, balance.target = balance.target, optimizer = "mosek", allow.negative.weights = TRUE, use.dual = FALSE, zeta = 0.9)
@@ -71,8 +84,11 @@ imbalance.mosek.free.zeta9 = t(M) %*% gamma.mosek.free.zeta9 - balance.target
 gamma.mosek.free2.zeta9 = approx.balance(M, balance.target = balance.target, optimizer = "mosek", allow.negative.weights = TRUE, use.dual = TRUE, zeta = 0.9)
 imbalance.mosek.free2.zeta9 = t(M) %*% gamma.mosek.free2.zeta9 - balance.target
 
-gamma.pogs.free.zeta9 = approx.balance(M, balance.target = balance.target, optimizer = "pogs", allow.negative.weights = TRUE, zeta = 0.9)
+gamma.pogs.free.zeta9 = approx.balance(M, balance.target = balance.target, optimizer = "pogs", allow.negative.weights = TRUE, use.dual = TRUE, zeta = 0.9)
 imbalance.pogs.free.zeta9 = t(M) %*% gamma.pogs.free.zeta9 - balance.target
+
+gamma.pogs.free2.zeta9 = approx.balance(M, balance.target = balance.target, optimizer = "pogs", allow.negative.weights = TRUE, use.dual = FALSE, zeta = 0.9)
+imbalance.pogs.free2.zeta9 = t(M) %*% gamma.pogs.free2.zeta9 - balance.target
 
 gamma.qp.free.zeta9 = approx.balance(M, balance.target = balance.target, optimizer = "quadprog", allow.negative.weights = TRUE, zeta = 0.9)
 imbalance.qp.free.zeta9 = t(M) %*% gamma.qp.free.zeta9 - balance.target
@@ -84,6 +100,8 @@ test_that("zeta tunes problem in right direction", {
   expect_true(sum(gamma.mosek.free2.zeta9^2) > sum(gamma.mosek.free2^2))
   expect_true(max(abs(imbalance.pogs.free.zeta9)) < max(abs(imbalance.pogs.free)))
   expect_true(sum(gamma.pogs.free.zeta9^2) > sum(gamma.pogs.free^2))
+  expect_true(max(abs(imbalance.pogs.free2.zeta9)) < max(abs(imbalance.pogs.free2)))
+  expect_true(sum(gamma.pogs.free2.zeta9^2) > sum(gamma.pogs.free2^2))
   expect_true(max(abs(imbalance.qp.free.zeta9)) < max(abs(imbalance.qp.free)))
   expect_true(sum(gamma.qp.free.zeta9^2) > sum(gamma.qp.free^2))
 })
